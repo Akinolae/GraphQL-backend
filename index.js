f65f2f679623606ca63a0791bd547354dc3e722b
Akinolae <makindeakinola22@gmail.com>
2022-11-29 16:38:58 +0100

    Update apollo-lambda.zip, cloudformation-template-create-stack.json, and 6 more files...

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ import { error as graphQlError } from "./utils/errorUtils.js";
 import { dbConfig } from "./config/dbConfig.js";
 import { ApolloServer } from "@apollo/server";
 import { verifyUserAccessToken } from "./utils/validator.js";
-import { startStandaloneServer } from "@apollo/server/standalone";
+// import { startStandaloneServer } from "@apollo/server/standalone";
 import { ApolloServerPluginCacheControl } from "@apollo/server/plugin/cacheControl";
+import { startServerAndCreateLambdaHandler } from "@as-integrations/aws-lambda";
 
 const server = new ApolloServer({
   typeDefs: schemas,
@@ -19,33 +20,14 @@ const server = new ApolloServer({
   ],
 });
 
-const { url } = await startStandaloneServer(server, {
-  context: async ({ req, res }) => {
-    let user;
-    if (!req.headers.authorization)
-      throw graphQlError("No token provided", {
-        extensions: {
-          code: "UNAUTHENTICATED",
-          http: { status: 401 },
-        },
-      });
-    else {
-      const token = req.headers.authorization.split(" ")[1];
-      try {
-        user = verifyUserAccessToken(token);
-      } catch (error) {
-        throw graphQlError(error, {
-          extensions: {
-            code: "UNAUTHENTICATED",
-            http: { status: 401 },
-          },
-        });
-      }
-    }
-    return { user };
+export const graphqlHandler = startServerAndCreateLambdaHandler(server, {
+  context: async ({ event, context }) => {
+    return {
+      lambdaEvent: event,
+      lambdaContext: context,
+    };
   },
-  listen: { port: 4000 },
 });
 
 dbConfig();
-console.log(`app is running ${url}`);
+console.log(`app is running`);
